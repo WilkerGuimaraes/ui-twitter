eb133ff8d60f4e0694c6199b6f9d9423f0bcc078
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-29 15:53:22 -0300

    feat: add new answer status

--- src/pages/status_/Status.tsx
+++ src/pages/status_/Status.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import { PaperPlaneRight } from "phosphor-react";
 
 import { StatusContainer } from "./Status.style";
@@ -7,23 +8,38 @@ import { Tweet } from "../../components/tweet_/Tweet";
 
 import profile9 from "../../assets/profile9.jpg";
 
-const answers = [
-  "Concordo...",
-  "Olha, faz sentido!",
-  "Parabéns pelo progresso.",
-];
-
 export const Status = () => {
+  const [newAnswer, setNewAnswer] = useState("");
+  const [answers, setAnswers] = useState([
+    "Concordo...",
+    "Olha, faz sentido!",
+    "Parabéns pelo progresso.",
+  ]);
+
+  const createNewAnswer = (event: FormEvent) => {
+    event.preventDefault();
+
+    setAnswers([newAnswer, ...answers]);
+    setNewAnswer("");
+  };
+
   return (
     <StatusContainer>
       <Header title="Tweet" />
 
       <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est officiis id delectus. Quasi sunt reprehenderit esse, obcaecati deleniti, vel deserunt accusantium amet magni blanditiis quos repellat velit sint voluptate!" />
 
-      <form className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src={profile9} alt="profile9" />
-          <textarea id="tweet" placeholder="Tweet your answer"></textarea>
+          <textarea
+            id="tweet"
+            placeholder="Tweet your answer"
+            value={newAnswer}
+            onChange={(event) => {
+              setNewAnswer(event.target.value);
+            }}
+          ></textarea>
         </label>
         <button type="submit">
           <PaperPlaneRight />
