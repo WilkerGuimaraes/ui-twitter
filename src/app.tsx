f444e2b5939b3ecd7f76b1f56143bfcf7120192a
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-29 11:42:43 -0300

    feat: add form and Separator component

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,25 @@ import { Content, Layout } from "./app.style";
 import { Header } from "./components/header_/Header";
 import { Sidebar } from "./components/sidebar_/Sidebar";
 
+import profile9 from "./assets/profile9.jpg";
+import { Separator } from "./components/separator_/Separator";
+
 export const App = () => {
   return (
     <Layout>
       <Sidebar />
       <Content>
         <Header title="Home" />
+
+        <form className="new-tweet-form">
+          <label htmlFor="tweet">
+            <img src={profile9} alt="profile9" />
+            <textarea id="tweet" placeholder="What's happening?"></textarea>
+          </label>
+          <button type="submit">Tweet</button>
+        </form>
+
+        <Separator />
       </Content>
     </Layout>
   );
