9efb73149b27cfbff50c2a3b820ef5e478a4650a
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-29 15:23:00 -0300

    feat: add new tweet status

--- src/pages/timeline_/Timeline.tsx
+++ src/pages/timeline_/Timeline.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from "react";
+
 import { TimelineContainer } from "./Timeline.style";
 
 import { Separator } from "../../components/separator_/Separator";
@@ -6,17 +8,34 @@ import { Header } from "../../components/header_/Header";
 
 import profile9 from "../../assets/profile9.jpg";
 
-const tweets = ["Meu primeiro tweet", "Teste", "Deu certo tweetar!"];
-
 export const Timeline = () => {
+  const [newTweet, setNewTweet] = useState("");
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "Teste",
+    "Deu certo tweetar!",
+  ]);
+
+  const createNewTweet = (event: FormEvent) => {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet("");
+  };
+
   return (
     <TimelineContainer>
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src={profile9} alt="profile9" />
-          <textarea id="tweet" placeholder="What's happening?"></textarea>
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onChange={(event) => setNewTweet(event.target.value)}
+          ></textarea>
         </label>
         <button type="submit">Tweet</button>
       </form>
